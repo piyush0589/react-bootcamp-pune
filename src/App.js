9c225e40970a2e0fca60da5838079d91170fbc00
piyush0589 <p.piyush123@gmail.com>
2024-06-04 12:41:17 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         
+         <h1>Piyush</h1>
         </p>
       </header>
     </div>
